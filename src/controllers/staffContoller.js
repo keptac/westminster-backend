c48a04dd534e10a3128f9cf76b01beb88685f317
keptac <keptac.dev@gmail.com>
2021-10-06 08:56:19 +0200

    status codes

--- src/controllers/staffContoller.js
+++ src/controllers/staffContoller.js
@@ -83,11 +83,11 @@ exports.staffAuthentication = async function (req, res) {
             }
             res.status(201).json({success:true, message:'Authentication successful', user:userBody})
         }else{
-            res.status(401).json({success:false, message:'Invalid email or password. ', error:"Invalid Email or password"})
+            res.json({success:false, message:'Invalid email or password. ', error:"Invalid Email or password"})
         }
     } catch (err) {
         console.log(err);
-        res.status(400).send({success:false,message:"Snap, something happened. Please contact the Admin or your helpdesk.", error:error});
+        res.send({success:false,message:"Snap, something happened. Please contact the Admin or your helpdesk.", error:error});
     }
 };
 
